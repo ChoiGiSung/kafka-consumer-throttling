6ab596260082bd6e69dca0a70d12892ffb58fd73
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2025-04-08 23:35:52 +0900

    add test code
    - 임베디드 카프카

--- src/test/kotlin/com/coco/throttling/ThrottlingApplicationTests.kt
+++ src/test/kotlin/com/coco/throttling/ThrottlingApplicationTests.kt
@@ -1,13 +1,41 @@
 package com.coco.throttling
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.test.context.EmbeddedKafka
 
+
+@EnableAutoConfiguration
 @SpringBootTest
+@EmbeddedKafka(
+    partitions = 6,
+    topics = [KafkaThrottledListener.TOPIC],
+    brokerProperties = [
+        "listeners=PLAINTEXT://localhost:9092",
+        "port=9092",
+        "max.poll.records=5",
+        "auto.create.topics.enable=true",
+    ]
+)
 class ThrottlingApplicationTests {
+    @Autowired
+    lateinit var kafkaTemplate: KafkaTemplate<String, String>
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun consumerThrottlingTest() {
+        (0..100).forEach { sendMessage(it) }
+        Thread.sleep(10000)
+    }
 
+
+    private fun sendMessage(i: Int) {
+        try {
+            val result = kafkaTemplate.send(KafkaThrottledListener.TOPIC, i.toString(), "message$i").get()
+            val partition = result.recordMetadata.partition()
+        } catch (_: Exception) {
+        }
+    }
 }
